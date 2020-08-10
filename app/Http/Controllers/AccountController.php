1c0b62314aba61a3d9adee3b3eaf626fa741735c
skyfury2651 <skyfury2651@gmail.com>
2020-08-10 09:53:21 +0700

    heroku

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -16,7 +16,8 @@ class AccountController extends Controller
     }
     public function admin_index(){
         $cities = City::all();
-        $accounts = Account::paginate(5);
+        $account_cur = Session::get('current_account');
+        $accounts = Account::where('id','!=',$account_cur->id)->paginate(5);
         return view('admin.accounts.account_list',compact('cities','accounts'));
     }
 
@@ -92,5 +93,68 @@ class AccountController extends Controller
         Session::forget('current_account');
 
         return redirect('/');
+    }
+
+    public function edit($id){
+        $account_cur = Session::get('current_account');
+        $account = Account::where('id','=',$id)->where('status','=','1')->where('id','!=',$account_cur->id)->first();
+        $cities = City::where('status','=','1')->get();
+        return view('admin.accounts.edit',compact('account','cities'));
+    }
+    public function update(Request $request, $id){
+        $request->validate([
+            'fullName' => 'required',
+            'email' => 'required',
+            'birthDate' => 'required',
+            'phoneNumber' => 'required',
+            'status' => 'required',
+            'city' => 'required',
+            'role' => 'required',
+        ]);
+        $account = Account::find($id);
+        $account->fullName = $request->fullName;
+        $account->email = $request->email;
+        $account->birthDate = $request->birthDate;
+        $account->status = $request->status;
+        dd($request);
+        $account->save();
+        return redirect(route('admin_account'));
+    }
+    public function create(){
+        $cities = City::all();
+        return view('admin.accounts.create',compact('cities'));
+    }
+    public function store(Request $request){
+
+        $request->validate([
+            'name' => 'required',
+        ],[
+            'name.required' => 'Tên hãng là cần thiết',
+        ]);
+
+        $account = new Account();
+//        dd($request);
+        $account->name = $request->name;
+        $account->save();
+        return redirect(route('admin_account'));
+    }
+    public function delete($id){
+        $account = Account::find($id);
+        $account->status = 0;
+        $account->save();
+        return redirect(route('admin_origin'));
+    }
+    public function delete_multi(Request $request){
+        $ids_array = new Array_();
+        $ids = $request->ids;
+        $ids_array = explode(',', $ids);
+//        return response()->json(['success'=>$ids_array]);
+        Account::whereIn('id', $ids_array)->update(['status' => 0]);
+
+        return response()->json(['success'=>"Account Deleted successfully."]);
+//        $products_array = $request->accounts;
+        //dd($products_array);
+        //check product con ton` tai hay khong
+//
     }
 }
